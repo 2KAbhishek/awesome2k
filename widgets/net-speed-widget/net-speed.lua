08457e31f6fb594276171bc7be1727f536b6f1ca
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-09-14 15:48:44 +0530

    Tweak up speed width

--- widgets/net-speed-widget/net-speed.lua
+++ widgets/net-speed-widget/net-speed.lua
@@ -66,7 +66,7 @@ local function worker(user_args)
         },
         {
             id = 'tx_speed',
-            forced_width = width,
+            forced_width = width - 10,
             align = 'left',
             widget = wibox.widget.textbox
         },
