6163747b4d56f6fab5619f770873d9a1a3534f59
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-10 10:34:04 +0530

    Update battery icons path

--- widgets/battery/battery.lua
+++ widgets/battery/battery.lua
@@ -17,7 +17,7 @@ local function worker(user_args)
     local args = user_args or {}
 
     local font = args.font or 'Play 8'
-    local path_to_icons = args.path_to_icons or "/usr/share/icons/Arc/status/symbolic/"
+    local path_to_icons = args.path_to_icons or WIDGET_DIR .. "/icons/"
     local show_current_level = args.show_current_level or true
     local margin_left = args.margin_left or 0
     local margin_right = args.margin_right or 0
