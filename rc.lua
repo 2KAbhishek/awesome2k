9c45f5fd2a431048902f992eb550729f74bcd74e
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-29 03:17:12 +0530

    Cleanup unused code

--- rc.lua
+++ rc.lua
@@ -87,26 +87,7 @@ local todo_widget = require("widgets.todo.todo")
 
 local round_rect = function(cr, w, h) gears.shape.rounded_rect(cr, w, h, 10) end
 
-local function set_wallpaper()
-    awful.util.spawn("nitrogen --restore")
-    -- Wallpaper
-    -- if beautiful.wallpaper then
-    --     local wallpaper = beautiful.wallpaper
-    --     -- If wallpaper is a function, call it with the screen
-    --     if type(wallpaper) == "function" then
-    --         wallpaper = wallpaper(s)
-    --     end
-    --     gears.wallpaper.maximized(wallpaper, s, true)
-    -- end
-end
-
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
-
 awful.screen.connect_for_each_screen(function(screen)
-    -- Wallpaper
-    -- set_wallpaper(s)
-
     -- Each screen has its own tag table.
     awful.tag({ " ", " ", " ", " ", " ", " ", " " }, screen, awful.layout.layouts[1])
 
@@ -206,10 +187,6 @@ end)
 
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function(c)
-    -- Set the windows at the slave,
-    -- i.e. put it at the end of others instead of setting it master.
-    -- if not awesome.startup then awful.client.setslave(c) end
-
     if awesome.startup
         and not c.size_hints.user_position
         and not c.size_hints.program_position then
