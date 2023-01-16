ec7832bc4709171264bf89da3fc57746db2129b5
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-01-16 15:49:09 +0530

    add: keybindings for scrot

--- keys.lua
+++ keys.lua
@@ -187,6 +187,12 @@ keys.global_keys = gears.table.join(
     awful.key({}, "Print",
         function() awful.util.spawn("scrot -e 'mv $f ~/Pictures/Screenshots/ 2>/dev/null'", false) end,
         { description = "take a screenshot", group = "client" }),
+    awful.key({ modkey, "Control" }, "Print",
+        function() awful.util.spawn("scrot -u -e 'mv $f ~/Pictures/Screenshots/ 2>/dev/null'", false) end,
+        { description = "take window screenshot", group = "client" }),
+    awful.key({ modkey }, "Print",
+        function() awful.util.spawn("scrot -s -e 'mv $f ~/Pictures/Screenshots/ 2>/dev/null'", false) end,
+        { description = "take regional screenshot", group = "client" }),
     -- Volume Keys
     awful.key({}, "XF86AudioLowerVolume", function()
         awful.util.spawn("amixer -q -D pulse sset Master 5%-", false)
